409fff5f8d714147d0b97305e80b88a11b0fbfc6
jwzheng <zhengjw@mail.ustc.edu.cn>
2019-10-21 14:45:01 +0800

    第三次提交，测试modify_gpu_bak是否还存在

--- run.py
+++ run.py
@@ -40,6 +40,7 @@ def run(opt):
     train.train(net,train_loader,test_loader,opt)
 
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir2label_dict",type=dict,default={'1':0,'2':1,'3':2,'4':3,'5':4,'6':5,'7':6,'8':7,'9':8,'10':9,'11':10,'12':11,'13':12,'14':13,'15':14,'16':15,'17':16,'18':17,'19':18,
